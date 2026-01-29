efb854f9ab83a72b530207f4bdb2ab328ea44a07
Alejandro Bolaño <alejandro.bolano@col.vueling.com>
2026-01-29 17:45:47 +0100

    feat: add minimum window size handling in message handler

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -187,6 +187,18 @@ Win32Window::MessageHandler(HWND hwnd,
       }
       return 0;
 
+    case WM_GETMINMAXINFO: {
+      MINMAXINFO* minMaxInfo = reinterpret_cast<MINMAXINFO*>(lparam);
+      POINT target_point = {0, 0};
+      HMONITOR monitor = MonitorFromPoint(target_point, MONITOR_DEFAULTTOPRIMARY);
+      UINT dpi = FlutterDesktopGetDpiForMonitor(monitor);
+      double scale_factor = dpi / 96.0;
+      
+      minMaxInfo->ptMinTrackSize.x = Scale(720, scale_factor);
+      minMaxInfo->ptMinTrackSize.y = Scale(720, scale_factor);
+      return 0;
+    }
+
     case WM_DPICHANGED: {
       auto newRectSize = reinterpret_cast<RECT*>(lparam);
       LONG newWidth = newRectSize->right - newRectSize->left;
